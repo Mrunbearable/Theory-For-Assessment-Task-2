3055ad2c847c5b0dab9b0242535f6577971b5982
Mrunbearable <stephen.wirianata@education.nsw.gov.au>
2025-05-20 14:39:08 +1000

    Update weapons1.py

--- 4. Abstraction/weapons1.py
+++ 4. Abstraction/weapons1.py
@@ -1,10 +1,27 @@
 from abc import ABC, abstractmethod
 
+
 class Weapon:
     def __init__(self, name, category, damage):
         self.name = name
         self.category = category
         self.damage = damage
 
-    def get_stats(self)
-        print("")
+    @abstractmethod
+    def get_stats(self):
+        pass
+        
+    def set_stats(self):
+        pass
+
+class Sword:
+    def __init__(self, name, category, damage, melee_type):
+        super().__init__(name, category, damage)
+        self.melee_type = melee_type
+
+    def get_stats(self):
+        print(f"Name: {self.name}")
+        print(f"Category: {self.category}")
+        print(f"Damage: {self.damage}")
+        print(f"Melee-xType: {self.melee_type}")
+        
